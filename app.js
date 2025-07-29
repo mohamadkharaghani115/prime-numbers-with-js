b59cf5ffcd882611cedd305af4b0a7148c9cea02
mohamadkharaghani115 <mohamadkharaghani115@gmail.com>
2025-07-30 00:21:08 +0330

    save these number

---
+++ app.js
@@ -0,0 +1,19 @@
+let num = 2;
+while (num <= 100) {
+    let isPrime = true; 
+    let i = 2;  
+     
+    while (i < num) {
+        if (num % i === 0) {
+            isPrime = false; 
+            break;
+        }
+        i++; 
+    }
+
+    if (isPrime) {
+        console.log(num);
+    }
+
+    num++;
+}
